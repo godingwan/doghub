2d7c4582fdf9f3fed2976fb859c118c2067dc2c4
Joshua Lee <jsungholee@gmail.com>
2013-05-09 16:04:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doghub::Application.config.secret_token = 'd00aae3086777c3c15f94c3cf86652d910b49a1a35b0fbde8f2a400bda1b6b46021e164d2ab5ccd4a165655a43d5945fa9f51bd151ffb5ca66c936eff53dc9c5'
